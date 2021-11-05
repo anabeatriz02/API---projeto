ea15da466cebe2deb54b96fbe6e9963512cf0c05
ana bea <anabeatrizpbrito02@gmail.com>
2021-11-05 10:59:21 -0300

    atualizar

--- crud.php
+++ crud.php
@@ -43,16 +43,42 @@ function readID($cod_pessoa, $conn) {
 ##FUNÇÃO DE INSERÇÃO##
 
 function create($nome, $sobrenome, $email, $celular, $fotgrafia, $conn) {
-    $sql = "INSERT INTO tbl_pessoa (nome, sobrenome, email, celular, fotografia
-            VALUES ('$nome', '$sobrenome','$email','$celular', '$fotgrafia')";
+    $sql = "INSERT INTO tbl_pessoa (nome, sobrenome, email, celular, fotografia) VALUES ('$nome', '$sobrenome','$email','$celular', '$fotgrafia')";
 
     if (mysqli_query($conn, $sql)) {
         echo json_encode(array("status"=>"success", "data"=>"Dados inseridos com sucesso"));
     } else {
-        echo json_encode(array("status"=>"error", "data"=>"Erro ao inserir os dados"));
+        echo json_encode(array("status"=>"error", "data"=>"Erro ao inserir os dados: " . mysqli_error($conn)));
+        // echo json_encode(array("status"=>"error", "data"=>"$sql"));
 
     }
 }
 
+##FUNÇÃO DE ATUALIZAÇÃO
+function update($cod_pessoa, $nome, $sobrenome, $email, $celular, $fotografia, $conn){
+
+    $sql = "UPDATE tbl_pessoa SET nome = '$nome', sobrenome = '$sobrenome', email = '$email', celular = '$celular', fotografia = '$fotografia' WHERE cod_pessoa = $cod_pessoa";
+
+    if (mysqli_query($conn, $sql)){
+        echo json_encode(array("status"=>"success", "data"=>"Dados alterados com sucesso"));
+    }else {
+        echo json_encode(array("status"=>"error", "data"=> $sql));
+
+    }
+
+}
+
+##FUNÇÃO DE EXCLUSÃO
+function delete($cod_pessoa, $conn){
+
+    $sql = "DELETE FROM tbl_pessoa WHERE cod_pessoa = $cod_pessoa";
+
+    if (mysqli_query($conn, $sql)){
+        echo json_encode(array("status"=>"success", "data"=>"Dados excluidos com sucesso"));
+    }else {
+        echo json_encode(array("status"=>"error", "data"=> $sql));
+
+    }
+}
 
 ?>
--- index.php
+++ index.php
@@ -37,9 +37,24 @@ if($acao == "create") {
     create($nome, $sobrenome, $email, $celular, $fotografia, $conn);
 }
 
+##ROTA DO UPDATE
+if($acao == "update") {
 
+    $cod_pessoa = $_REQUEST["cod_pessoa"];
+    $nome = $_REQUEST["nome"];
+    $sobrenome = $_REQUEST["sobrenome"];
+    $email = $_REQUEST["email"];
+    $celular = $_REQUEST["celular"];
+    $fotografia = $_REQUEST["fotografia"];
 
+    update($cod_pessoa, $nome, $sobrenome, $email, $celular, $fotografia, $conn);
+}
 
+##ROTA DO UPDATE
+if($acao == "delete") {
 
+    $cod_pessoa = $_REQUEST["cod_pessoa"];
+    delete($cod_pessoa, $conn);
+}
 
 ?>
