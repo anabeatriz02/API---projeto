104f286e05ea470a8f6d9b9b5623a15868017f66
ana bea <anabeatrizpbrito02@gmail.com>
2021-11-05 09:13:42 -0300

    novo

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: GET, POST");
+header("Content-type: application/json");
+
+include('connection.php');
+include('crud.php');
+
+##RECUPERA O TIPO DE AÇÃO DE REQUISIÇÃO##
+$acao = $_REQUEST["acao"];
+
+##ROTA DO READ##
+if($acao == "read") {
+
+    read($conn);
+}
+
+if($acao == "readID") {
+
+    $cod_pessoa = $_REQUEST["cod_pessoa"];
+    readID($cod_pessoa, $conn);
+}
+
+
+##ROTA DO CREATE##
+
+if($acao == "create") {
+
+    $nome = $_REQUEST["nome"];
+    $sobrenome = $_REQUEST["sobrenome"];
+    $email = $_REQUEST["email"];
+    $celular = $_REQUEST["celular"];
+    $fotografia = $_REQUEST["fotografia"];
+
+    create($nome, $sobrenome, $email, $celular, $fotografia, $conn);
+}
+
+
+
+
+
+
+?>
